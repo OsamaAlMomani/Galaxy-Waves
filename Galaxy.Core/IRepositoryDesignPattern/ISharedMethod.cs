8c235d06bfc7b4ed22b29d224ce134beded47f4c
OsamaAlMomani <momani.322.44157@gmail.com>
2023-04-25 02:19:46 +0300

    REBUILD 2.1

--- Galaxy.Core/IRepositoryDesignPattern/ISharedMethod.cs
+++ Galaxy.Core/IRepositoryDesignPattern/ISharedMethod.cs
@@ -6,20 +6,14 @@ using System.Threading.Tasks;
 
 namespace Galaxy.Core.IRepositoryDesignPattern
 {
-<<<<<<< HEAD
-    public interface ISharedMethod <T> where T : class
+    public interface ISharedMethod<T> where T : class
     {
-        public IEnumerable<T> GetAllAsync(T entity);
+        public IEnumerable<T> GetAllAsync(T entitsy);
         public Task<T> GetByIdAsync(Guid id);
         public Task<T> AddAsync(T entity);
         public Task<T> UpdateAsync(T entity);
         public Task<T> SoftDeleteAsync(T entity);
         public Task<T> HardDeleteAsync(T entity);
 
-
-=======
-    internal interface ISharedMethod
-    {
->>>>>>> 9c8cd6270ab129e0a49971a768fdf8ba8a048ef9
     }
 }
