e9e18409d865f66c9e7fc9e0d0f7d29819cf8f69
OsamaAlMomani <momani.322.44157@gmail.com>
2023-05-04 16:35:36 +0300

    Style Sheets Added

---
+++ Main/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Main.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        // GET: DashboardController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
