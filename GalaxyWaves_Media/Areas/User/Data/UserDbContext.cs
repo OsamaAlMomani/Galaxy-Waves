3882717bd3d4292bc393b3efb09593cde237150c
osama <osamamomani9560@outlook.com>
2023-07-07 01:47:53 +0300

    Rebuild 7.1V

---
+++ GalaxyWaves_Media/Areas/User/Data/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GalaxyWaves_Media.Areas.User.Data
+{
+    public class UserDbContext:DbContext
+    {
+        public UserDbContext(DbContextOptions <UserDbContext>options) : base(options)
+        {
+        }
+
+         
+    }
+}
---
+++ GalaxyWaves_Media/Areas/admin/Models/RoleManager/CreateRole.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GalaxyWaves_Media.Areas.admin.Models.RoleManager
+{
+    public class CreateRole
+    {
+        [Key] public int RoleId { get; set; }
+        [Required] public string? RoleName { get; set; }
+    }
+}
