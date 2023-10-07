1e300d365ebf2c58cc6226d676b3bb3e9da51caf
Okay Okay :) <osamamomani9560@outlook.com>
2023-10-08 01:53:41 +0300

    Galaxy-Wave Virsion 2

---
+++ GalaxyWave.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GalaxyWave.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- GalaxyWaves/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GalaxyWaves.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
